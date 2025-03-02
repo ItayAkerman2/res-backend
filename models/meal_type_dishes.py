0dca15bb738c7ab687b58458aca15af14963dc0b
Amit-Bam <amitbamberger@gmail.com>
2025-03-02 14:35:55 +0200

    db

--- models/meal_type_dishes.py
+++ models/meal_type_dishes.py
@@ -4,3 +4,4 @@ class Meal_Type_Dishes(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     dishID = db.Column(db.Integer, db.ForeignKey('dishes.dishID'),nullable=False)
     meal_type_ID = db.Column(db.Integer,db.ForeignKey('meal_type.id'),nullable = False)
+    

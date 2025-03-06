1c4b36b355bbea4aba777c9c737641ff7c4b7947
Kevin-Mansherov <kevinmansherov28@gmail.com>
2025-03-06 12:29:46 +0200

    add to employees

--- models/employees.py
+++ models/employees.py
@@ -6,3 +6,6 @@ class Employees(db.Model):
     last_name=db.Column(db.String(50),nullable=False)
     pay_per_hour=db.Column(db.Numeric(precision = 10,scale = 2),nullable = False)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.role_id'))
+    phone_number = db.Column(db.String(10))
+    email = db.Column(db.String(10))
+    birth_date = db.Column(db.DateTime)

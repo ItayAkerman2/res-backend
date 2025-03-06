97501836c085b867689f89f8ff4f6f74199880a9
Kevin-Mansherov <kevinmansherov28@gmail.com>
2025-03-06 13:05:27 +0200

    remove nullable

--- models/dishes.py
+++ models/dishes.py
@@ -2,10 +2,10 @@ from . import db
 
 class Dishes(db.Model):
     dishID = db.Column(db.Integer,primary_key = True)
-    dishName = db.Column(db.String(50),nullable = False)
-    cost = db.Column(db.Numeric(precision = 10,scale = 2),nullable = False)
-    cook_time = db.Column(db.DateTime,nullable = False)
-    image_url = db.Column(db.String(255),nullable=False)
+    dishName = db.Column(db.String(50))
+    cost = db.Column(db.Numeric(precision = 10,scale = 2))
+    cook_time = db.Column(db.DateTime)
+    image_url = db.Column(db.String(255))
     description = db.Column(db.String(255))
     to_show = db.Column(db.Integer)
     created_at = db.Column(db.Date)

7c9b92c77f5d535128740fca92eb2e4d024ff159
Kevin-Mansherov <kevinmansherov28@gmail.com>
2025-03-06 13:11:44 +0200

    Update dishes.py

--- models/dishes.py
+++ models/dishes.py
@@ -4,7 +4,7 @@ class Dishes(db.Model):
     dishID = db.Column(db.Integer,primary_key = True)
     dishName = db.Column(db.String(50))
     cost = db.Column(db.Numeric(precision = 10,scale = 2))
-    cook_time = db.Column(db.DateTime)
+    cook_time = db.Column(db.TIMESTAMP())
     image_url = db.Column(db.String(255))
     description = db.Column(db.String(255))
     to_show = db.Column(db.Integer)
